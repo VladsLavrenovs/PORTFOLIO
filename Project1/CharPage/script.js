328d04020a567480045c08ff22120ea66461ba1c
Vlads Lavrenovs <vlads.lavrenovs@gmail.com>
2024-02-27 21:34:54 +0200

    redone the first small poart of char page

--- Project1/CharPage/script.js
+++ Project1/CharPage/script.js
@@ -1,37 +1,14 @@
-const list = document.querySelectorAll(".list");
+var lastPanelId = '';
 
-function activeLink() {
-    list.forEach((item) => {
-        item.classList.remove("active");
-    })
-    this.classList.add("active")
+function toggleInfoPanel(panelId) {
+    var panel = document.getElementById(panelId);
+        if (panel.style.top === '-4000px' || lastPanelId !== panelId) {
+            panel.style.top = '0';
+            if (lastPanelId && lastPanelId !== panelId) {
+                document.getElementById(lastPanelId).style.top = '-4000px';
+            }
+        } else {
+            panel.style.top = '-4000px';
+        }
+        lastPanelId = panelId;
 }
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const playerMenuItems = document.querySelectorAll('.list');
-
-    playerMenuItems.forEach(item => {
-        item.addEventListener('click', function () {
-            const playerTextContainer = document.getElementById('playerTextContainer');
-            const playerImageContainer = document.getElementById('playerImageContainer');
-            const playerName = item.querySelector('.text').textContent;
-            const playerFileName = playerName.toLowerCase().replace(/\s/g, ''); // Convert name to lowercase and remove spaces
-
-            // Fetch and update text content
-            fetch(`../Players/${playerFileName}.txt`)
-                .then(response => response.text())
-                .then(textContent => {
-                    playerTextContainer.textContent = textContent;
-                })
-                .catch(error => console.error('Error fetching text content:', error));
-
-            // Update player image
-            const playerImageSrc = item.querySelector('.icon img').src;
-            const playerImage = document.createElement('img');
-            playerImage.src = playerImageSrc;
-            playerImageContainer.innerHTML = ''; // Clear previous content
-            playerImageContainer.appendChild(playerImage);
-        });
-    });
-});
